e1df4989558bc85706b09fa81e0a681ff4e21133
MarioBroi <inoix16@gmail.com>
2024-02-16 13:31:04 +0000

    edit main.js -Completed Exercise 2

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,3 +4,20 @@
 
 const number = 100; //this variable *number* will contain the number *100*
 console.log(number);
+
+//EX 2
+
+const pi = 3.14; //this variable *pi* will contain the number *3.14*
+console.log(pi);
+
+//EX 2/B
+
+/* 
+
+const pi = 3.1416;  
+console.log(pi);
+
+In this case i'm reciving an error from the console because the variable *pi* is a constant and cannot be defined only once. 
+To fix this problem i can modify my variable const whit a variable log
+
+*/
