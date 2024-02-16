9e4fbe16bad888e1a99e6d3da5820c2db7e79b2f
MarioBroi <inoix16@gmail.com>
2024-02-16 13:50:18 +0000

    edit main.js -Completed Exercise 3

--- assets/js/main.js
+++ assets/js/main.js
@@ -27,3 +27,9 @@ To fix this problem i can modify my variable const whit a variable let
 let radius = 8; //this variable will contain the value: 8 
 let circle = ((radius*2)*pi); //this variable will multiply the variable 8 for 2 times (8*2=16) and multiply the total for the constant pi (16*3.14=50.24)
 console.log (circle);
+
+//EX 3
+
+let name = "Carlo" //this variable will display the string "Carlo"
+name = "Marco" //this variable will display the string "Marco" and "Carlo" gonna be overwritten 
+console.log(name);
